adfab1acc034cc3d1d5a894b50b6e51fabcacd6b
Johannes Wesseler <142618752+JohannesWes@users.noreply.github.com>
2025-05-06 09:26:48 +0200

    Refactoring, Pipelining, Synthesis hint

--- pyrpl/fpga/rtl/red_pitaya_asg_ch.v
+++ pyrpl/fpga/rtl/red_pitaya_asg_ch.v
@@ -36,73 +36,82 @@
 
 module red_pitaya_asg_ch #(
    parameter RSZ = 14,
+   parameter DACBITS = 14,
    parameter CYCLE_BITS = 32,
    parameter PHASEBITS = 32,
    parameter USE_EXT_PHASE = 0
 )(
    // DAC
-   output reg [ 14-1: 0] dac_o           ,  //!< dac data output
-   input                 dac_clk_i       ,  //!< dac clock
-   input                 dac_rstn_i      ,  //!< dac reset - active low
+   output reg [ DACBITS-1: 0] dac_o           ,  //!< dac data output
+   input                      dac_clk_i       ,  //!< dac clock
+   input                      dac_rstn_i      ,  //!< dac reset - active low
    // trigger
-   input                 trig_sw_i       ,  //!< software trigger
-   input                 trig_ext_i      ,  //!< external trigger
-   input      [  3-1: 0] trig_src_i      ,  //!< trigger source selector
-   output                trig_done_o     ,  //!< trigger event
+   input                      trig_sw_i       ,  //!< software trigger
+   input                      trig_ext_i      ,  //!< external trigger
+   input      [  3-1: 0]      trig_src_i      ,  //!< trigger source selector
+   output                     trig_done_o     ,  //!< trigger event
 
    // external phase
    input      [PHASEBITS-1:0] asg_phase_ext     ,  // phase input - allow for directly controlling the current phase - not the step size
-   
+
    // buffer ctrl
-   input                 buf_we_i        ,  //!< buffer write enable
-   input      [ 14-1: 0] buf_addr_i      ,  //!< buffer address
-   input      [ 14-1: 0] buf_wdata_i     ,  //!< buffer write data
-   output reg [ 14-1: 0] buf_rdata_o     ,  //!< buffer read data
-   output reg [RSZ-1: 0] buf_rpnt_o      ,  //!< buffer current read pointer
+   input                      buf_we_i        ,  //!< buffer write enable
+   input      [ RSZ-1: 0]     buf_addr_i      , //!< buffer address
+   input      [ RSZ-1: 0]     buf_wdata_i     ,  //!< buffer write data
+   output reg [ RSZ-1: 0]     buf_rdata_o     ,  //!< buffer read data
+   output reg [ RSZ-1: 0]     buf_rpnt_o      ,  //!< buffer current read pointer
 
    // configuration
-   input     [RSZ+16-1: 0] set_size_i      ,  //!< set table data size
-   input     [RSZ+16-1: 0] set_step_i      ,  //!< set pointer step
-   input     [RSZ+16-1: 0] set_ofs_i       ,  //!< set reset offset
-   input                 set_rst_i       ,  //!< set FSM to reset
-   input                 set_once_i      ,  //!< set only once  -- not used
-   input                 set_wrap_i      ,  //!< set wrap enable
-   input     [  14-1: 0] set_amp_i       ,  //!< set amplitude scale
-   input     [  14-1: 0] set_dc_i        ,  //!< set output offset
-   input                 set_zero_i      ,  //!< set output to zero
-   input     [  CYCLE_BITS-1: 0] set_ncyc_i      ,  //!< set number of cycle
-   input     [  16-1: 0] set_rnum_i      ,  //!< set number of repetitions
-   input     [  32-1: 0] set_rdly_i      ,  //!< set delay between repetitions
-   input                 set_rgate_i     ,  //!< set external gated repetition
+   input     [RSZ+16-1: 0]    set_size_i    ,  //!< set table data size
+   input     [RSZ+16-1: 0]    set_step_i    ,  //!< set pointer step
+
+   input     [RSZ+16-1: 0]    set_ofs_i     ,  //!< set reset offset
+
+   input     [ DACBITS-1: 0]  set_amp_i       ,  //!< set amplitude scale
+
+   input                      set_rst_i       ,  //!< set FSM to reset
+   input                      set_once_i      ,  //!< set only once  -- not used
+   input                      set_wrap_i      ,  //!< set wrap enable
+
+   input     [ DACBITS-1: 0]  set_dc_i        ,  //!< set output offset
+   input                      set_zero_i      ,  //!< set output to zero
+   input     [  CYCLE_BITS-1: 0] set_ncyc_i   ,  //!< set number of cycle
+   input     [  16-1: 0]      set_rnum_i      ,  //!< set number of repetitions
+   input     [  32-1: 0]      set_rdly_i      ,  //!< set delay between repetitions
+   input                      set_rgate_i     ,  //!< set external gated repetition
 
 
-   input                 rand_on_i     , // random number generator on
-   input     [RSZ-1:0]   rand_pnt_i      // random pointer for output data
+   input                      rand_on_i       ,  // random number generator on
+   input     [RSZ-1:0]        rand_pnt_i         // random pointer for output data
 
 );
 
 //---------------------------------------------------------------------------------
 //
 //  DAC buffer RAM
-
+(* ram_style = "block" *) // Synthesis hint
 reg   [  14-1: 0] dac_buf [0:(1<<RSZ)-1] ;
-reg   [  14-1: 0] dac_rd    ;
-reg   [  14-1: 0] dac_rdat  ;
-reg   [ RSZ-1: 0] dac_rp    ;
-reg   [ RSZ-1: 0] dac_rp_reg; // Registered version of dac_rp for stable indexing
-reg   [RSZ+16-1: 0] phase_sum ;
-reg   [RSZ+16-1: 0] dac_pnt   ; // read pointer - fractional bits for sub-sample precision
-reg   [RSZ+16-1: 0] dac_pntp  ; // previous read pointer
-wire  [RSZ+17-1: 0] dac_npnt  ; // next read pointer
-wire  [RSZ+17-1: 0] dac_npnt_sub ;
+
+// --- Read path logic  ---
+reg   [  14-1: 0] dac_rd;
+reg   [  14-1: 0] dac_rdat;
+reg   [ RSZ-1: 0] dac_rp;
+reg   [ RSZ-1: 0] dac_rp_reg;    // Registered version of dac_rp for stable indexing
+reg   [RSZ+16-1: 0] phase_sum;
+reg   [RSZ+16-1: 0] dac_pnt;     // read pointer - fractional bits for sub-sample precision
+reg   [RSZ+16-1: 0] dac_pntp;    // previous read pointer
+wire  [RSZ+17-1: 0] dac_npnt;    // next read pointer
+wire  [RSZ+17-1: 0] dac_npnt_sub;
 wire                dac_npnt_sub_neg;
 
-reg   [  28-1: 0] dac_mult  ;
-reg   [  15-1: 0] dac_sum   ;
+// --- Scaling and Offset logic ---
+reg   [  28-1: 0] dac_mult  , dac_mult_reg;
+reg   [  15-1: 0] dac_sum;
 
 // read from buffer
 always @(posedge dac_clk_i)
 begin
+   // Calculate read pointers
    if (USE_EXT_PHASE) begin // use externally supplied phase as read pointer
        phase_sum <= asg_phase_ext[PHASEBITS-1:PHASEBITS-RSZ-16] + set_ofs_i[RSZ+16-1:0]; // use only the 30 MSBs from asg_phase_ext
        dac_rp <= phase_sum[RSZ+16-1:16]; 
@@ -130,7 +139,8 @@ if (buf_we_i)  dac_buf[buf_addr_i] <= buf_wdata_i[14-1:0] ;
 // scale and offset
 always @(posedge dac_clk_i)
 begin
-   dac_mult <= $signed(dac_rdat) * $signed({1'b0,set_amp_i}) ;
+   dac_mult_reg <= $signed(dac_rdat) * $signed({1'b0,set_amp_i});
+   dac_mult <= dac_mult_reg;
    dac_sum  <= $signed(dac_mult[28-1:13]) + $signed(set_dc_i) ;
 
    // saturation
@@ -225,6 +235,7 @@ end
 
 assign dac_trig = (!dac_rep && trig_in) || (dac_rep && |rep_cnt && (dly_cnt == 32'h0)) ;
 // check if we are at the end of the waveform buffer (computes difference between next pointer and size of the waveform buffer) & check if negative
+// Calculate wrap conditions
 assign dac_npnt_sub = dac_npnt - {1'b0,set_size_i} - 1;
 assign dac_npnt_sub_neg = dac_npnt_sub[RSZ+16];
 
