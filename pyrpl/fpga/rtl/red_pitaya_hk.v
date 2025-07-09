0a6232e95f5797dde1c8e961ff1eb8d3b83d568c
Johannes Wesseler <142618752+JohannesWes@users.noreply.github.com>
2025-07-09 13:23:03 +0200

    Enhance HouseKeeping module with source selection and pin configuration features
    Previously, the digital output values could only be set via the system-bus (and thus only via software/python). Now the red_pitaya_hk.v module accepts inputs for these values from other FPGA modules, such that other FPGA modules can control the output values on the expansion connector.
    The choice between setting the values via software or via other FPGA modules is enabled with selectors that can be controlled from python.

--- pyrpl/fpga/rtl/red_pitaya_hk.v
+++ pyrpl/fpga/rtl/red_pitaya_hk.v
@@ -1,29 +1,9 @@
 /**
- * $Id: red_pitaya_hk.v 961 2014-01-21 11:40:39Z matej.oblak $
+ * @brief Red Pitaya house keeping module
  *
- * @brief Red Pitaya house keeping.
- *
- * @Author Matej Oblak
- *
- * (c) Red Pitaya  http://www.redpitaya.com
- *
- * This part of code is written in Verilog hardware description language (HDL).
- * Please visit http://en.wikipedia.org/wiki/Verilog
- * for more details on the language used herein.
- */
-
-/**
- * GENERAL DESCRIPTION:
- *
- * House keeping module takes care of system identification.
- *
- *
- * This module takes care of system identification via DNA readout at startup and
- * ID register which user can define at compile time.
- *
- * Beside that it is currently also used to test expansion connector and for
- * driving LEDs.
- * 
+ * This module handles system identification and digital IO control.
+ * Digital outputs can be controlled via system bus or from other FPGA modules.
+ * Source selection (system bus vs module input) is configurable per pin.
  */
 
 module red_pitaya_hk #(
@@ -32,19 +12,27 @@ module red_pitaya_hk #(
   parameter [57-1:0] DNA = 57'h0823456789ABCDE
 )(
   // system signals
+
   input                clk_i      ,  // clock
   input                rstn_i     ,  // reset - active low
   // LED
   output reg [DWL-1:0] led_o      ,  // LED output
+
   // global configuration
   output reg           digital_loop,
-  // Expansion connector
-  input      [DWE-1:0] exp_p_dat_i,  // exp. con. input data
-  output reg [DWE-1:0] exp_p_dat_o,  // exp. con. output data
-  output reg [DWE-1:0] exp_p_dir_o,  // exp. con. 1-output enable
-  input      [DWE-1:0] exp_n_dat_i,  //
-  output reg [DWE-1:0] exp_n_dat_o,  //
-  output reg [DWE-1:0] exp_n_dir_o,  //
+
+  // Expansion connector - direct connection to physical pins
+  input      [DWE-1:0] exp_p_dat_i,  // exp. con. P input data
+  output reg [DWE-1:0] exp_p_dat_o,  // exp. con. P output data
+  output reg [DWE-1:0] exp_p_dir_o,  // exp. con. P direction (1=output, 0=input)
+  input      [DWE-1:0] exp_n_dat_i,  // exp. con. N input data
+  output reg [DWE-1:0] exp_n_dat_o,  // exp. con. N output data
+  output reg [DWE-1:0] exp_n_dir_o,  // exp. con. N direction (1=output, 0=input)
+
+  // Module inputs - data from other FPGA modules
+  input      [DWE-1:0] mod_exp_p_dat,  // exp. con. P data from modules
+  input      [DWE-1:0] mod_exp_n_dat,  // exp. con. N data from modules
+
   // System bus
   input      [ 32-1:0] sys_addr   ,  // bus address
   input      [ 32-1:0] sys_wdata  ,  // bus write data
@@ -57,8 +45,20 @@ module red_pitaya_hk #(
 );
 
 //---------------------------------------------------------------------------------
-//
-//  Read device DNA
+// Internal registers for system bus control
+
+// Data and direction from system bus
+reg [DWE-1:0] sys_exp_p_dat;
+reg [DWE-1:0] sys_exp_p_dir;
+reg [DWE-1:0] sys_exp_n_dat;
+reg [DWE-1:0] sys_exp_n_dir;
+
+// Source selection registers (0=system bus, 1=module input)
+reg [DWE-1:0] exp_p_src_sel;  // Source select for P expansion
+reg [DWE-1:0] exp_n_src_sel;  // Source select for N expansion
+
+//---------------------------------------------------------------------------------
+// Read device DNA
 
 wire           dna_dout ;
 reg            dna_clk  ;
@@ -101,7 +101,6 @@ DNA_PORT #(.SIM_DNA_VALUE (DNA)) i_DNA (
 );
 
 //---------------------------------------------------------------------------------
-//
 //  Design identification
 
 wire [32-1: 0] id_value;
@@ -110,53 +109,115 @@ assign id_value[31: 4] = 28'h0; // reserved
 assign id_value[ 3: 0] =  4'h1; // board type   1 - release 1
 
 //---------------------------------------------------------------------------------
-//
-//  System bus connection
+// Output multiplexing - select between system bus and module inputs
+
+integer i;
+
+always @(posedge clk_i) begin
+  if (rstn_i == 1'b0) begin
+    exp_p_dat_o <= {DWE{1'b0}};
+    exp_p_dir_o <= {DWE{1'b0}};
+    exp_n_dat_o <= {DWE{1'b0}};
+    exp_n_dir_o <= {DWE{1'b0}};
+  end else begin
+    // Multiplexing for P expansion
+    for (i = 0; i < DWE; i = i + 1) begin
+      exp_p_dat_o[i] <= exp_p_src_sel[i] ? mod_exp_p_dat[i] : sys_exp_p_dat[i];
+      exp_p_dir_o[i] <= exp_p_src_sel[i] ? 1'b1 : sys_exp_p_dir[i]; // Module data is always output, not input
+    end
+    
+    // Multiplexing for N expansion
+    for (i = 0; i < DWE; i = i + 1) begin
+      exp_n_dat_o[i] <= exp_n_src_sel[i] ? mod_exp_n_dat[i] : sys_exp_n_dat[i];
+      exp_n_dir_o[i] <= exp_n_src_sel[i] ? 1'b1 : sys_exp_n_dir[i]; // Module data is always output, not input
+    end
+  end
+end
+
+//---------------------------------------------------------------------------------
+// System bus interface
 
 always @(posedge clk_i)
 if (rstn_i == 1'b0) begin
-  led_o        <= {DWL{1'b0}};
-  exp_p_dat_o  <= {DWE{1'b0}};
-  exp_p_dir_o  <= {DWE{1'b0}};
-  exp_n_dat_o  <= {DWE{1'b0}};
-  exp_n_dir_o  <= {DWE{1'b0}};
-end else if (sys_wen) begin
-  if (sys_addr[19:0]==20'h0c)   digital_loop <= sys_wdata[0];
+  led_o            <= {DWL{1'b0}};
+  digital_loop     <= 1'b0;
 
-  if (sys_addr[19:0]==20'h10)   exp_p_dir_o  <= sys_wdata[DWE-1:0];
-  if (sys_addr[19:0]==20'h14)   exp_n_dir_o  <= sys_wdata[DWE-1:0];
-  if (sys_addr[19:0]==20'h18)   exp_p_dat_o  <= sys_wdata[DWE-1:0];
-  if (sys_addr[19:0]==20'h1C)   exp_n_dat_o  <= sys_wdata[DWE-1:0];
-
-  if (sys_addr[19:0]==20'h30)   led_o        <= sys_wdata[DWL-1:0];
+  sys_exp_p_dat    <= {DWE{1'b0}};
+  sys_exp_p_dir    <= {DWE{1'b0}};
+  sys_exp_n_dat    <= {DWE{1'b0}};
+  sys_exp_n_dir    <= {DWE{1'b0}};
+  exp_p_src_sel    <= {DWE{1'b0}};
+  exp_n_src_sel    <= {DWE{1'b0}};
+end else begin
+  // Handle system bus writes
+  if (sys_wen) begin
+    case (sys_addr[19:0])
+      20'h0C: digital_loop  <= sys_wdata[0];
+      
+      // Direction control
+      20'h10: sys_exp_p_dir <= sys_wdata[DWE-1:0];
+      20'h14: sys_exp_n_dir <= sys_wdata[DWE-1:0];
+      
+      // Data output
+      20'h18: sys_exp_p_dat <= sys_wdata[DWE-1:0];
+      20'h1C: sys_exp_n_dat <= sys_wdata[DWE-1:0];
+      
+      // Source selection (0=system, 1=module)
+      20'h40: exp_p_src_sel <= sys_wdata[DWE-1:0];
+      20'h44: exp_n_src_sel <= sys_wdata[DWE-1:0];
+      
+      // LED control
+      20'h30: led_o         <= sys_wdata[DWL-1:0];
+    endcase
+  end
 end
 
+// System bus read logic
 wire sys_en;
 assign sys_en = sys_wen | sys_ren;
 
 always @(posedge clk_i)
 if (rstn_i == 1'b0) begin
-  sys_err <= 1'b0;
-  sys_ack <= 1'b0;
+  sys_err   <= 1'b0;
+  sys_ack   <= 1'b0;
+  sys_rdata <= 32'h0;
 end else begin
   sys_err <= 1'b0;
+  sys_ack <= sys_en;
 
-  casez (sys_addr[19:0])
-    20'h00000: begin sys_ack <= sys_en;  sys_rdata <= {                id_value          }; end
-    20'h00004: begin sys_ack <= sys_en;  sys_rdata <= {                dna_value[32-1: 0]}; end
-    20'h00008: begin sys_ack <= sys_en;  sys_rdata <= {{64- 57{1'b0}}, dna_value[57-1:32]}; end
-    20'h0000c: begin sys_ack <= sys_en;  sys_rdata <= {{32-  1{1'b0}}, digital_loop      }; end
-
-    20'h00010: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_p_dir_o}       ; end
-    20'h00014: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_n_dir_o}       ; end
-    20'h00018: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_p_dat_o}       ; end
-    20'h0001C: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_n_dat_o}       ; end
-    20'h00020: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_p_dat_i}       ; end
-    20'h00024: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWE{1'b0}}, exp_n_dat_i}       ; end
-
-    20'h00030: begin sys_ack <= sys_en;  sys_rdata <= {{32-DWL{1'b0}}, led_o}             ; end
-
-      default: begin sys_ack <= sys_en;  sys_rdata <=  32'h0                              ; end
+  case (sys_addr[19:0])
+    // Identification
+    20'h00000: sys_rdata <= id_value;
+    20'h00004: sys_rdata <= dna_value[32-1:0];
+    20'h00008: sys_rdata <= {{32-25{1'b0}}, dna_value[57-1:32]};
+    
+    // Configuration
+    20'h0000c: sys_rdata <= {{32-1{1'b0}}, digital_loop};
+    
+    // Direction registers
+    20'h00010: sys_rdata <= {{32-DWE{1'b0}}, sys_exp_p_dir};
+    20'h00014: sys_rdata <= {{32-DWE{1'b0}}, sys_exp_n_dir};
+    
+    // Output data registers - values set by the system bus
+    20'h00018: sys_rdata <= {{32-DWE{1'b0}}, sys_exp_p_dat};
+    20'h0001C: sys_rdata <= {{32-DWE{1'b0}}, sys_exp_n_dat};
+    
+    // Input data (read actual pin values)
+    20'h00020: sys_rdata <= {{32-DWE{1'b0}}, exp_p_dat_i};
+    20'h00024: sys_rdata <= {{32-DWE{1'b0}}, exp_n_dat_i};
+    
+    // LED register
+    20'h00030: sys_rdata <= {{32-DWL{1'b0}}, led_o};
+    
+    // Source selection registers
+    20'h00040: sys_rdata <= {{32-DWE{1'b0}}, exp_p_src_sel};
+    20'h00044: sys_rdata <= {{32-DWE{1'b0}}, exp_n_src_sel};
+    
+    // Current output values (after mux)
+    20'h00048: sys_rdata <= {{32-DWE{1'b0}}, exp_p_dat_o};
+    20'h0004C: sys_rdata <= {{32-DWE{1'b0}}, exp_n_dat_o};
+    
+    default: sys_rdata <= 32'h0;
   endcase
 end
 
--- pyrpl/hardware_modules/hk.py
+++ pyrpl/hardware_modules/hk.py
@@ -1,10 +1,11 @@
 from ..attributes import IntRegister, SelectRegister, IORegister, BoolProperty
 from ..modules import HardwareModule
 from ..widgets.module_widgets.hk_widget import HkWidget
-import numpy as np
 
 
 class ExpansionDirection(BoolProperty):
+    """Property for controlling expansion pin direction"""
+
     def set_value(self, obj, val):
         obj._set_expansion_direction(self.name.strip('_output'), val)
 
@@ -12,45 +13,77 @@ class ExpansionDirection(BoolProperty):
         return obj._get_expansion_direction(self.name.strip('_output'))
 
 
+class SourceSelect(BoolProperty):
+    """Property for controlling pin source (system bus vs external module input)"""
+
+    def set_value(self, obj, val):
+        obj._set_source_select(self.name.strip('_src'), val)
+
+    def get_value(self, obj):
+        return obj._get_source_select(self.name.strip('_src'))
+
+
 class HK(HardwareModule):
+    """
+    HouseKeeping module for RedPitaya
+
+    Controls digital I/O expansion connectors, LEDs, and provides system identification.
+    Each expansion pin can be controlled either by the system bus (Python) or by
+    other FPGA modules, selectable on a per-pin basis.
+    """
     _widget_class = HkWidget
 
     _setup_attributes = ["led"] + \
                         ['expansion_P' + str(i) for i in range(8)] + \
-                        ['expansion_P' + str(i) + '_output' for i in range(8)]+ \
+                        ['expansion_P' + str(i) + '_output' for i in range(8)] + \
+                        ['expansion_P' + str(i) + '_src' for i in range(8)] + \
                         ['expansion_N' + str(i) for i in range(8)] + \
-                        ['expansion_N' + str(i) + '_output' for i in range(8)]
-    _gui_attributes =  _setup_attributes
+                        ['expansion_N' + str(i) + '_output' for i in range(8)] + \
+                        ['expansion_N' + str(i) + '_src' for i in range(8)]
+
+    _gui_attributes = _setup_attributes
     addr_base = 0x40000000
-    # We need all attributes to be there when the interpreter is done reading the class (for metaclass to workout)
-    # see http://stackoverflow.com/questions/2265402/adding-class-attributes-using-a-for-loop-in-python
+
+    # System identification
+    id = SelectRegister(0x0, doc="Device ID")
+    dna_low = IntRegister(0x04, doc="Device DNA (low 32 bits)")
+    dna_high = IntRegister(0x08, doc="Device DNA (high 25 bits)", bits=25)
+
+    # Configuration
+    digital_loop = IntRegister(0x0C, doc="Enables digital loop", bits=1, min=0, max=1)
+
+    # LED control
+    led = IntRegister(0x30, doc="LED control with bits 0:7", min=0, max=2 ** 8 - 1)
+
+    # Expansion connector registers - dynamically created
+    # Create P and N expansion pin registers
     for i in range(8):
         locals()['expansion_P' + str(i)] = IORegister(0x20, 0x18, 0x10, bit=i,
                                                       outputmode=True,
-                                                      doc="positive digital io")
+                                                      doc=f"Positive expansion pin {i}")
         locals()['expansion_P' + str(i) + '_output'] = ExpansionDirection(
-                                                      doc="direction of the "
-                                                          "port")
+            doc=f"Direction of positive expansion pin {i} (True=output, False=input)")
+        locals()['expansion_P' + str(i) + '_src'] = SourceSelect(
+            doc=f"Source select for positive expansion pin {i} (True=module, False=system)")
+
+        # N expansion
         locals()['expansion_N' + str(i)] = IORegister(0x24, 0x1C, 0x14, bit=i,
                                                       outputmode=True,
-                                                      doc="positive digital io")
+                                                      doc=f"Negative expansion pin {i}")
         locals()['expansion_N' + str(i) + '_output'] = ExpansionDirection(
-                                                      doc="direction of the "
-                                                          "port")
+            doc=f"Direction of negative expansion pin {i} (True=output, False=input)"
+        )
+        locals()['expansion_N' + str(i) + '_src'] = SourceSelect(
+            doc=f"Source select for negative expansion pin {i} (True=module, False=system)"
+        )
 
-    id = SelectRegister(0x0, doc="device ID", options={"prototype0": 0,
-                                                       "release1": 1})
-    digital_loop = IntRegister(0x0C, doc="enables digital loop")
-    led = IntRegister(0x30, doc="LED control with bits 1:8", min=0, max=2**8)
-    # another option: access led as array of bools
-    # led = [BoolRegister(0x30,bit=i,doc="LED "+str(i)) for i in range(8)]
+    # Source selection registers (full byte access)
+    _p_source_select = IntRegister(0x40, doc="P expansion source select (bit per pin)", min=0, max=255)
+    _n_source_select = IntRegister(0x44, doc="N expansion source select (bit per pin)", min=0, max=255)
 
-    def set_expansion_direction(self, index, val):
-        """Sets the output mode of expansion index (both for P and N expansions)"""
-        if not index in range(8):
-            raise ValueError("Index from 0 to 7 expected")
-        for name in ["expansion_P", "expansion_N"]:
-            getattr(HK, name + str(index)).direction(self, val)
+    # Current output values (read-only, shows values after mux)
+    _p_current_output = IntRegister(0x48, doc="Current P expansion output values")
+    _n_current_output = IntRegister(0x4C, doc="Current N expansion output values")
 
     def _setup(self): # the function is here for its docstring to be used by the metaclass.
         """
@@ -59,15 +92,90 @@ class HK(HardwareModule):
         pass
 
     def _set_expansion_direction(self, name, val):
-        """Sets the output mode of expansion index (both for P and N expansions)"""
-        #if not index in range(8):
-        #    raise ValueError("Index from 0 to 7 expected")
-        #for name in ["expansion_P", "expansion_N"]:
+        """Set the direction for an expansion pin"""
         getattr(HK, name).direction(self, val)
 
     def _get_expansion_direction(self, name):
-        """Sets the output mode of expansion index (both for P and N expansions)"""
-        #if not index in range(8):
-            #raise ValueError("Index from 0 to 7 expected")
-        return getattr(HK, name).outputmode# direction(self,
-        # val)
+        """Get the direction for an expansion pin"""
+        # Get the IORegister instance
+        io_register = getattr(self.__class__, name)
+        # Return the current outputmode setting
+        return io_register.outputmode
+
+    def _set_source_select(self, name, val):
+        """Set source selection for a single pin"""
+        # Extract pin info from name (e.g., "expansion_P3" -> type="P", index=3)
+        parts = name.split('_')
+        exp_type = parts[1][0]  # 'P' or 'N'
+        index = int(parts[1][1:])  # pin number
+
+        # Get current register value
+        if exp_type == 'P':
+            current = self._p_source_select
+        else:
+            current = self._n_source_select
+
+        # Set or clear the bit
+        if val:
+            new_value = current | (1 << index)
+        else:
+            new_value = current & ~(1 << index)
+
+        # Write back
+        if exp_type == 'P':
+            self._p_source_select = new_value
+        elif exp_type == 'N':
+            self._n_source_select = new_value
+        else:
+            raise ValueError(f"Invalid pin type in name: {name}. Expected 'P' or 'N'.")
+
+    def _get_source_select(self, name):
+        """Get source selection for a single pin"""
+        # Extract pin info from name
+        parts = name.split('_')
+        exp_type = parts[1][0]  # 'P' or 'N'
+        index = int(parts[1][1:])  # pin number
+
+        # Read register and extract bit
+        if exp_type == 'P':
+            return bool((self._p_source_select >> index) & 1)
+        elif exp_type == 'N':
+            return bool((self._n_source_select >> index) & 1)
+        else :
+            raise ValueError(f"Invalid pin type in name: {name}. Expected 'P' or 'N'.")
+
+
+    def configure_pin(self, pin_name, direction='output', source='system'):
+        """
+        Configure a single pin's direction and source.
+
+        Parameters
+        ----------
+        pin_name : str
+            Pin name (e.g., 'P0', 'N7', 'expansion_P0')
+        direction : str
+            'input' or 'output'
+        source : str
+            'system' or 'module'
+
+        Example
+        -------
+        >>> hk.configure_pin('P0', direction='output', source='module')
+        >>> hk.configure_pin('N3', direction='input')
+        """
+        # Normalize pin name
+        if not pin_name.startswith('expansion_'):
+            if pin_name[0] in ['P', 'N']:
+                pin_name = f'expansion_{pin_name}'
+            else:
+                raise ValueError(f"Invalid pin name: {pin_name}")
+
+        # Set direction
+        dir_attr = pin_name + '_output'
+        if hasattr(self, dir_attr):
+            setattr(self, dir_attr, direction == 'output')
+
+        # Set source
+        src_attr = pin_name + '_src'
+        if hasattr(self, src_attr):
+            setattr(self, src_attr, source == 'module')
